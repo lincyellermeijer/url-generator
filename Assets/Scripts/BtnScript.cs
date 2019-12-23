784a4f2ff111b329265ab0b3690709fc8e5f0cf4
Lincy Ellermeijer <lincy_95@hotmail.com>
2019-12-23 18:30:34 +0100

    V1.0

---
+++ Assets/Scripts/BtnScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BtnScript : MonoBehaviour
+{
+    public enum Permission { Denied = 0, Granted = 1, ShouldAsk = 2 };
+
+    private string path;
+   private ReadWriteTXT handleText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        handleText = new ReadWriteTXT();
+    }
+
+    public void UploadTxt()
+    {
+        //path = EditorUtility.OpenFilePanel("Overwrite with txt", "", "txt");
+        GetTxt();
+    }
+
+    private void GetTxt()
+    {
+        Debug.Log("the following text was uploaded");
+        handleText.ReadString(path);
+    }
+
+    public void SetTxt()
+    {
+        handleText.WriteString(path, "1/2/12345");
+    }
+
+    public void btnOne()
+    {
+        handleText.WriteString("Assets/Resources/test.txt", "test");
+    }
+
+    public void btnTwo()
+    {
+        handleText.ReadString("Assets/Resources/test.txt");
+    }
+
+    public void btnQuit()
+    {
+        Application.Quit();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/Explorer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using SmartDLL;
+
+public class Explorer : MonoBehaviour
+{
+    public Text eText;
+    public Button openExplorerButton;
+    public Button generateURLButton;
+
+    public SmartFileExplorer fileExplorer = new SmartFileExplorer();
+
+    private bool readText = false;
+
+    void OnEnable()
+    {
+        openExplorerButton.onClick.AddListener(delegate { ShowExplorer(); });
+        generateURLButton.onClick.AddListener(delegate { Generate(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fileExplorer.resultOK && readText)
+        {
+            ReadText(fileExplorer.fileName);
+            readText = false;
+        }
+    }
+
+    void ShowExplorer()
+    {
+        string initialDir = @"C:\";
+        bool restoreDir = true;
+        string title = "Open a Text File";
+        string defExt = "txt";
+        string filter = "txt files (*.txt)|*.txt";
+
+        fileExplorer.OpenExplorer(initialDir, restoreDir, title, defExt, filter);
+        readText = true;
+    }
+
+    void Generate()
+    {
+        GenerateURL(fileExplorer.fileName, eText.text);
+        readText = true;
+        // productcode uitlezen en invullen
+    }
+
+    void ReadText(string path)
+    {
+        eText.text = File.ReadAllText(path);
+    }
+
+    void GenerateURL(string path, string productCode)
+    {
+        //Write some text to the uploaded .txt file
+        //StreamWriter writer = new StreamWriter(path, true);
+        using (StreamWriter writer = File.CreateText(path))
+        {
+            writer.WriteLine("https://www.123inkt.nl/images/products/" + productCode[0] + "/" + productCode[1] + "/" + productCode + "/" + productCode + "_1_big.jpg");
+            writer.Close();
+        }
+    }
+}
